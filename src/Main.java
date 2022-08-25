ab172e37b3ff2815754270cbfb0053cb32c0979e
Duonghieu97st <Duonghieu97st@gmail.com>
2022-08-25 15:21:52 +0700

    readFile

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = null;
+        try {
+            String line;
+            bufferedReader = new BufferedReader(new FileReader("countries.csv"));
+            while ((line = bufferedReader.readLine()) != null) {
+                printCountry(parseCsvLine(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null)
+                    bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    public static List<String> parseCsvLine(String csvLine) {
+        List<String> result = new ArrayList<>();
+        if (csvLine != null) {
+            String[] splitData = csvLine.split(",");
+            for (int i = 0; i < splitData.length; i++) {
+                result.add(splitData[i]);
+            }
+        }
+        return result;
+    }
+
+    private static void printCountry(List<String> country) {
+        System.out.println(
+                "Country [id= "
+                        + country.get(0)
+                        + ", code= " + country.get(1)
+                        + " , name=" + country.get(2)
+                        + "]");
+    }
+}
